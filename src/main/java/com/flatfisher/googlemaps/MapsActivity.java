eb1352168261ef6db1ba5cff3c7e5747fe6828b7
Kyohei Tsuda <shimano.entou@gmail.com>
2015-08-12 07:28:19 +0900

    Initial commit.

---
+++ src/main/java/com/flatfisher/googlemaps/MapsActivity.java
@@ -0,0 +1,99 @@
+package com.flatfisher.googlemaps;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity {
+
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private TextView centerLocationText;
+
+    private double zoomLower = 8;
+    private double zoomUpper = 17;
+    private double latitudeLower = 35;
+    private double latitudeUpper = 36;
+    private double longitudeLower = 139;
+    private double longitudeUpper = 140;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        setUpMapIfNeeded();
+        centerLocationText = (TextView)findViewById(R.id.center_location_textView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p/>
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p/>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p/>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        mMap.setMyLocationEnabled(true);
+        mMap.setTrafficEnabled(true);
+        mMap.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
+            @Override
+            public void onCameraChange(CameraPosition cameraPosition) {
+                double zoomLevel = cameraPosition.zoom;
+                double latitude = cameraPosition.target.latitude;
+                double longitude = cameraPosition.target.longitude;
+
+                String centerText = "zoom level " + zoomLevel + "\n" + "latitude "+latitude + "\n" + "longitude " + longitude;
+
+                centerLocationText.setText(centerText);
+
+                if (zoomLevel<=zoomUpper&&zoomLevel>=zoomLower&&
+                        latitude<=latitudeUpper&&latitude>=latitudeLower&&
+                        longitude<=longitudeUpper&&longitude>=longitudeLower){
+                    Toast.makeText(MapsActivity.this,"正解!",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+//        mMap.setMapType(GoogleMap.);
+    }
+}
